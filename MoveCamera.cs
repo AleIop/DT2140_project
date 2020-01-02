260f4c1e3f04abbd6ea046e9f7a0c7811ce20d36
Alessandro Iop <AleIop@users.noreply.github.com>
2020-01-02 16:20:54 +0100

    Update MoveCamera.cs
    Added comments

--- MoveCamera.cs
+++ MoveCamera.cs
@@ -1,52 +1,62 @@
 ﻿using UnityEngine;
- 
+
+
+/*
+	this class is for controlling the camera position and movements with the keyboard only
+*/
 public class MoveCamera : MonoBehaviour {
 
-	Vector3 topviewPosition = new Vector3(0f,24.56f,0f);
-	Vector3 initPosition;
+	Vector3 topviewPosition = new Vector3(0f,24.56f,0f); //camera position from above the scene
+	Vector3 initPosition; //initial camera position
  
-	private float speed = 5.0f;
+	private float speed = 5.0f; //movement speed when pressing the keys
+	
+	//these are probably meant to limit the movement range for some reason... mah
 	public float minX = -360.0f;
-	public float maxX = 360.0f;
-		
+	public float maxX = 360.0f;	
 	public float minY = -45.0f;
 	public float maxY = 45.0f;
 	
+	//sensitivity of X and Y axis when rotating the camera with the mouse
 	public float sensX = 1.0f;
 	public float sensY = 1.0f;
-		
+	
+	//initial rotation values when controlling the camera with the mouse, which get incremented while holding the C key 
 	float rotationY = 0.0f;
 	float rotationX = 0.0f;
 
-	void Awake()
-	{
+	void Awake() {
 		initPosition = transform.position;
 	}
 	void Update () {
 	
 		if (Input.GetKey(KeyCode.D)){
 			//transform.position += Vector3.right * speed * Time.deltaTime;
-			transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self); //RIGHT
+			transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self); //moves right when pressing D
 
 		}
+
 		if (Input.GetKey(KeyCode.A)){
 			//transform.position += Vector3.left * speed * Time.deltaTime;
-			transform.Translate(Vector3.left * Time.deltaTime * speed, Space.Self); //LEFT
+			transform.Translate(Vector3.left * Time.deltaTime * speed, Space.Self); //moves left when pressing A
 		}
+
 		if (Input.GetKey(KeyCode.Q)){
-			transform.position -= Vector3.up * speed * Time.deltaTime;
+			transform.position -= Vector3.up * speed * Time.deltaTime; //moves down when pressing Q
 		}
+
 		if (Input.GetKey(KeyCode.E)){
-			transform.position += Vector3.up * speed * Time.deltaTime;
+			transform.position += Vector3.up * speed * Time.deltaTime; //moves up when pressing E
 		}
+
 		if (Input.GetKey(KeyCode.W)){
 			//transform.position += Vector3.forward * speed * Time.deltaTime;
-			transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self); //FORWARD
+			transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self); //moves forward when pressing W
 		}
 
 		if (Input.GetKey(KeyCode.S)){
 			//transform.position += Vector3.back * speed * Time.deltaTime;
-			transform.Translate(Vector3.back * Time.deltaTime * speed, Space.Self); //BACK
+			transform.Translate(Vector3.back * Time.deltaTime * speed, Space.Self); //moves backward when pressing S
 		}
 		
 		//top down view
@@ -60,11 +70,11 @@ public class MoveCamera : MonoBehaviour {
 			transform.rotation= Quaternion.Euler(new Vector3(30,0,0));
 		}
 
-		//hold this in to rotate camera
-		if (Input.GetKey (KeyCode.C)) {
-			rotationY += Input.GetAxis ("Mouse Y") * sensY * Time.deltaTime;
-						rotationX += Input.GetAxis ("Mouse X") * sensX * Time.deltaTime;
-			transform.localEulerAngles = new Vector3 (-rotationY,rotationX, 0);
+		//hold C to rotate camera with the mouse
+		if (Input.GetKey(KeyCode.C)) {
+			rotationY += Input.GetAxis("Mouse Y")*sensY*Time.deltaTime;
+			rotationX += Input.GetAxis("Mouse X")*sensX*Time.deltaTime;
+			transform.localEulerAngles = new Vector3(-rotationY,rotationX, 0);
 		}
 
 	}
