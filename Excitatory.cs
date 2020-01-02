db9550c9ccc03260f64d1f098350873fc302d58c
Alessandro Iop <AleIop@users.noreply.github.com>
2020-01-02 16:08:51 +0100

    Update Excitatory.cs
    Added comments

--- Excitatory.cs
+++ Excitatory.cs
@@ -6,65 +6,61 @@ public class Excitatory : MonoBehaviour {
 	public int id;
 	public Color color;
     //public Material activeShader;
-   // public Material[] pyramidShader;
+    //public Material[] pyramidShader;
     public bool activation;
     public int latestActivationTime;
     //private Color excitatoryColor;
 
-
+    /*
+        this method initiates the single pyramid cell
+    */
     void Start()
     {
-        //Color myColor = pyramidShader[0].color;
-        //myColor.a = 0.1f;
-        //Color myColor2 = transform.parent.GetComponent<MC>().COLOR;
-        //myColor2.a = 1f;
-        //pyramidShader[0].color = myColor;
-        //pyramidShader[1].color = myColor2;
+        /*
+        Color myColor = pyramidShader[0].color;
+        myColor.a = 0.1f;
+        Color myColor2 = transform.parent.GetComponent<MC>().COLOR;
+        myColor2.a = 1f;
+        pyramidShader[0].color = myColor;
+        pyramidShader[1].color = myColor2;
+        */
 
         activation = false;
         latestActivationTime = 0;
 
-      // GetComponent<Renderer>().material = pyramidShader[0];
-       //GetComponent<Renderer>().material = pyramidShader[1];
+        //GetComponent<Renderer>().material = pyramidShader[0];
+        //GetComponent<Renderer>().material = pyramidShader[1];
     }
 
-    public Color COLOR
-	{
-		get
-		{
+
+    /*
+        properties for color, previous activation time, activation status of the single pyramid cell
+    */
+    public Color COLOR {
+		get {
 			return this.color;
 		}
-		set
-		{
+		set {
 			this.color = value;
 		}
 	}
-    public int PreviousTime
-    {
-        get
-        {
+    public int PreviousTime {
+        get {
             return this.latestActivationTime;
         }
-        set
-        {
+        set {
             this.latestActivationTime = value;
         }
     }
-    public bool IsActive
-    {
-        get
-        {
+    public bool IsActive {
+        get {
             return this.activation;
         }
-        set
-        {
-            if (this.activation)
-            {
+        set {
+            if (this.activation) {
                 //the animation call here
-               // StartCoroutine(DeactivateExc());
-            }
-            else
-            {
+                //StartCoroutine(DeactivateExc());
+            } else {
                 //StartCoroutine(ActivateExc());
             }
             this.activation = value;
