a8dc5fab3d06f9e5c7fd644664dbe62cdf2863f3
Alessandro Iop <AleIop@users.noreply.github.com>
2020-01-03 11:27:41 +0100

    Update PlayAnimation.cs
    Added comments

--- PlayAnimation.cs
+++ PlayAnimation.cs
@@ -8,9 +8,11 @@ public class PlayAnimation : MonoBehaviour {
 	//AnimationClip clip;
 	//AnimationCurve curve;
 	//public bool isActive = false;
-    private Color excitatoryColor;
+    private Color excitatoryColor; //color of the pyramid cells
 
     /* 
+    these methods were supposed to manage the color-change animation through keyframes
+
 	void Start()
 	{
 		anim = GetComponent<Animation>();
@@ -18,6 +20,7 @@ public class PlayAnimation : MonoBehaviour {
 		clip.legacy = true;
 
 	}
+
 	public void ChangeCol(){
 		//print("bruh");
 		Keyframe[] keys = new Keyframe[3];
@@ -31,9 +34,10 @@ public class PlayAnimation : MonoBehaviour {
 		anim.AddClip(clip, clip.name);
 		anim.Play(clip.name);
        	//transform.GetComponent<Renderer>().material.color = Color.red;
-	}*/
+	}
+    */
 
-
+    //this method was supposed to manage the color-change of basket cells as a function of the change speed
     public void ActivatedInhibitory(float speed){
 		//StartCoroutine(ActivateInh(speed));
 		//Invoke("InhibitoryNormalState", 1.1f-speed);
@@ -42,65 +46,58 @@ public class PlayAnimation : MonoBehaviour {
 		/*for(float i=0; i<1; i=i+0.1f){
 			StartCoroutine("ActivateInh");
 		}*/
-
 	}
-
-
     
-    public IEnumerator ActivateInh(float speed)
-    {
-            for (float i = 1; i <=10; i++)
-            {
-                transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.blue, i * 0.1f);
-            transform.localScale = Vector3.Lerp(CreateNeurons.basketScale, (CreateNeurons.basketScale * 2f), (i * 0.1f));
-
+    //these methods respectively changes color of basket cells from white to blue and enlarges them, and vice versa, when they are activated/deactivated, then wait for a given amount of time
+    public IEnumerator ActivateInh(float speed) {
+        for (float i = 1; i <=10; i++) {
+            transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.blue, i*0.1f);
+            transform.localScale = Vector3.Lerp(CreateNeurons.basketScale, (CreateNeurons.basketScale * 2f), (i*0.1f));
             yield return new WaitForSeconds((speed));
         }
     }
-    
     public IEnumerator DeactivateInh(float speed) {
-		for(float i=1; i<=10; i++){
+		for(float i=1; i<=10; i++) {
 			transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue,Color.white,i*0.1f);
             transform.localScale = Vector3.Lerp(CreateNeurons.basketScale * 2f, CreateNeurons.basketScale, (i * 0.1f));
-
             yield return new WaitForSeconds((speed));
         }
 	}
     
-
-
+    //these methods respectively changes color of pyramid cells from white to the color of the MC and enlarges them, and vice versa, when they are activated/deactivated, then wait for a given amount of time
     public IEnumerator ActivateExc(float speed) {
-            //float alpha = 0f;
-            for (float i = 1; i <= 10; i++) {
-              /*  if (i < 1)
-                    alpha = 0.1f;
-                else
-                    alpha = Mathf.Lerp(transform.GetComponent<Excitatory>().pyramidShader[0].color.a, 1f, i * 0.1f);
-                excitatoryColor = transform.parent.GetComponent<MC>().COLOR;
-                excitatoryColor.a = alpha;*/
+        //float alpha = 0f;
+        for (float i = 1; i <= 10; i++) {
+            /* 
+            if (i < 1)
+                alpha = 0.1f;
+            else
+                alpha = Mathf.Lerp(transform.GetComponent<Excitatory>().pyramidShader[0].color.a, 1f, i * 0.1f);
+            excitatoryColor = transform.parent.GetComponent<MC>().COLOR;
+            excitatoryColor.a = alpha;
+            */
 
-                //transform.GetComponent<Renderer>().material.color = Color.Lerp(transform.GetComponent<Excitatory>().pyramidShader[0].color, excitatoryColor, i * 0.1f);
-                transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, transform.parent.GetComponent<MC>().COLOR, i * 0.1f);
-                transform.localScale = Vector3.Lerp(CreateNeurons.pyramidScale, (CreateNeurons.pyramidScale * 2.5f), (i*0.1f));
-
+            //transform.GetComponent<Renderer>().material.color = Color.Lerp(transform.GetComponent<Excitatory>().pyramidShader[0].color, excitatoryColor, i * 0.1f);
+            
+            transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, transform.parent.GetComponent<MC>().COLOR, i * 0.1f);
+            transform.localScale = Vector3.Lerp(CreateNeurons.pyramidScale, (CreateNeurons.pyramidScale * 2.5f), (i*0.1f));
             yield return new WaitForSeconds((speed));
         }
     }
-
-
 	public IEnumerator DeactivateExc(float speed) {
-           // float alpha = 0f;
-            for (float i = 1; i <= 10; i++)
-            {
-                /*if (i < 1)
-                    alpha = 0.1f;
-                else
-                    alpha = Mathf.Lerp(transform.GetComponent<Excitatory>().pyramidShader[0].color.a, 1f, i * 0.1f);
-                excitatoryColor = transform.parent.GetComponent<MC>().COLOR;
-                excitatoryColor.a = alpha;*/
+        // float alpha = 0f;
+        for (float i = 1; i <= 10; i++) {
+            /*
+            if (i < 1)
+                alpha = 0.1f;
+            else
+                alpha = Mathf.Lerp(transform.GetComponent<Excitatory>().pyramidShader[0].color.a, 1f, i * 0.1f);
+            excitatoryColor = transform.parent.GetComponent<MC>().COLOR;
+            excitatoryColor.a = alpha;
+            */
 
-                transform.GetComponent<Renderer>().material.color = Color.Lerp(transform.parent.GetComponent<MC>().COLOR, Color.white, i * 0.1f);
-                transform.localScale = Vector3.Lerp(CreateNeurons.pyramidScale * 2.5f, CreateNeurons.pyramidScale, (i * 0.1f));
+            transform.GetComponent<Renderer>().material.color = Color.Lerp(transform.parent.GetComponent<MC>().COLOR, Color.white, i * 0.1f);
+            transform.localScale = Vector3.Lerp(CreateNeurons.pyramidScale * 2.5f, CreateNeurons.pyramidScale, (i * 0.1f));
 
             //transform.parent.GetComponent<Renderer>().material.color = Color.Lerp(transform.parent.GetComponent<MC>().COLOR, Color.white, i * 0.1f);
 
